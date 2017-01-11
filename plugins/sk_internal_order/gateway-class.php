56a5300238985e80aa4337f5316e55b853b4a85d
Johan Linder <johan@fmca.se>
2017-01-11 10:43:23 +0100

    Add internal order plugin

---
+++ plugins/sk_internal_order/gateway-class.php
@@ -0,0 +1,94 @@
+<?php
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+/**
+ * SKIOS Gateway class
+ */
+class SKIOS_Gateway extends WC_Payment_Gateway {
+
+	function __construct() {
+		$this->id                 = 'skios';
+		$this->icon               = '';
+		$this->has_fields         = false;
+		$this->title              = 'TITLE';
+		$this->method_title       = 'SK Internal Order System';
+		$this->method_description = 'Varje produkt tillhör en ägare och när ordern läggs går ett mejl iväg till respektive ägare om beställningen.';
+
+		$this->init_form_fields();
+		$this->init_settings();
+
+		$this->title              = $this->get_option( 'title' );
+
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+	}
+
+	function init_form_fields() {
+
+		$admin_email = get_bloginfo( 'admin_email');
+
+		$this->form_fields = array(
+			'enabled' => array(
+				'title' => __( 'Enable/Disable', 'woocommerce' ),
+				'type' => 'checkbox',
+				'label' => __( 'Enable Internal Order', 'skios' ),
+				'default' => 'yes'
+			),
+			'title' => array(
+				'title' => __( 'Title', 'woocommerce' ),
+				'type' => 'text',
+				'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce' ),
+				'default' => __( 'Internbeställning', 'woocommerce' ),
+				'desc_tip'      => true,
+			),
+			'product_owners' => array(
+				'title' => __( 'Produktägare (en ägare per rad i formatet: "Benämning, Epost"). Första raden används som standard på produkter som inte blivit tilldelad en ägare.', 'skios' ),
+				'type' => 'textarea',
+				'default' => 'Standard, '.$admin_email
+			)
+		);
+
+	}
+
+	function process_payment( $order_id ) {
+
+		global $woocommerce;
+
+		$order = new WC_Order( $order_id );
+
+		$owners = skios_get_product_owners();
+
+		$items = $order->get_items();
+
+		$sorted_items = array();
+
+		foreach ($items as $item) {
+			$product_id = $item['product_id'];
+			$owner = get_post_meta( $product_id, '_product_owner', true);
+
+			if (!$owner) $owner = skios_get_default_product_owner_email();
+
+			$sorted_items[$owner][] = $item;
+		}
+
+		skios_handle_order_notifications( $order, $sorted_items );
+
+		return false;
+
+		// Mark as on-hold (we're awaiting the cheque)
+		$order->update_status('wc-internal-order', __( 'Orderinfo skickat till produkternas ägare.', 'skios' ));
+
+		// Reduce stock levels
+		$order->reduce_order_stock();
+
+		// Remove cart
+		$woocommerce->cart->empty_cart();
+
+		// Return thankyou redirect
+		return array(
+			'result' => 'success',
+			'redirect' => $this->get_return_url( $order )
+		);
+	}
+
+}
---
+++ plugins/sk_internal_order/sk_internal_order.php
@@ -0,0 +1,159 @@
+<?php
+/*
+Plugin Name: SK Internal Order System
+Plugin URI:  utveckling.sundsvall.se
+Description: Description
+Version:     20170105
+Author:      SK/FMCA
+Author URI:  utveckling.sundsvall.se
+License:     ??
+License URI: ??
+Text Domain: skios
+Domain Path:
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+class SKIOS {
+
+	function __construct() {
+		// Add custom gateway
+		add_action( 'plugins_loaded', array($this, 'init_skios_gateway_class' ) );
+
+		// Add custom order status to be used in custom gateway.
+		add_action( 'init', array($this, 'register_internal_order_status' ) );
+		add_filter( 'wc_order_statuses', array( $this, 'add_internal_to_order_statuses' ) );
+
+		// Display and save product owner option on product admin screen.
+		add_action( 'woocommerce_product_options_general_product_data', array( $this, 'woo_add_custom_general_fields' ) );
+		add_action( 'woocommerce_process_product_meta', array( $this, 'woo_add_custom_general_fields_save' ) );
+
+	}
+
+	function init_skios_gateway_class() {
+		include_once __DIR__ . '/gateway-class.php';
+
+		add_filter( 'woocommerce_payment_gateways', array($this, 'add_your_gateway_class' ) );
+	}
+
+	function add_your_gateway_class( $methods ) {
+		$methods[] = 'SKIOS_Gateway';
+		return $methods;
+	}
+
+	/**
+	 * Add custom order status to be used by the gateway class after sending an
+	 * order to the product owners.
+	 */
+	function register_internal_order_status() {
+		register_post_status( 'wc-internal-order', array(
+				'label'                     => 'Intern beställning',
+				'public'                    => true,
+				'exclude_from_search'       => false,
+				'show_in_admin_all_list'    => true,
+				'show_in_admin_status_list' => true,
+				'label_count'               => _n_noop( 'Intern beställning <span class="count">(%s)</span>', 'Intern beställning <span class="count">(%s)</span>' )
+		) );
+	}
+
+	// Add to list of WC Order statuses
+	function add_internal_to_order_statuses( $order_statuses ) {
+
+			$new_order_statuses = array();
+
+			// add new order status after processing
+			foreach ( $order_statuses as $key => $status ) {
+
+					$new_order_statuses[ $key ] = $status;
+
+					if ( 'wc-processing' === $key ) {
+							$new_order_statuses['wc-internal-order'] = 'Intern beställning';
+					}
+			}
+
+			return $new_order_statuses;
+	}
+
+	/**
+	 * Add select to product where product owner are selected.
+	 */
+	function woo_add_custom_general_fields() {
+
+		global $woocommerce, $post;
+
+		$owners = skios_get_product_owners();
+
+		$options = array();
+
+		foreach ( $owners as $owner ) {
+
+			$name  = $owner[0];
+			$email = $owner[1];
+
+			$options[$email] = $name;
+		}
+
+
+		echo '<div class="options_group">';
+
+		// Select
+		woocommerce_wp_select(
+			array(
+				'id'      => '_product_owner',
+				'label'   => __( 'Produktägare', 'woocommerce' ),
+				'options' => $options
+			)
+		);
+
+		echo '</div>';
+	}
+
+	/**
+	 * Save product owner field
+	 */
+	function woo_add_custom_general_fields_save( $post_id ){
+
+		$product_owner = $_POST['_product_owner'];
+		if( !empty( $product_owner ) )
+			update_post_meta( $post_id, '_product_owner', esc_attr( $product_owner ) );
+
+	}
+
+}
+
+new SKIOS;
+
+/**
+ * Send email with order data to product owners. Only include the products that
+ * belong to the respective owner in each email.
+ */
+function skios_handle_order_notifications( $order, $sorted_items ) {
+	return false;
+}
+
+/**
+ * Get an array of availanle product owners (name and email).
+ */
+function skios_get_product_owners() {
+
+	$options = get_option( 'woocommerce_skios_settings', false );
+
+	$str = $options['product_owners'];
+
+	$arr = explode( "\n", $str );
+
+	foreach ( $arr as $key => $val ) {
+		$arr[$key] = array_map( 'trim', explode( ",", $val ) );
+	}
+
+	return $arr;
+
+}
+
+/**
+ * Get default (first) product owner.
+ */
+function skios_get_default_product_owner_email() {
+	$owners = skios_get_product_owners();
+	return $owners[0][1];
+}
