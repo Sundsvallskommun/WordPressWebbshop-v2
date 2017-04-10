967d98790f53763ba90ad7bc9df295316eade86b
Johan Linder <johan@fmca.se>
2017-04-10 14:52:16 +0200

    Only allow cart to contain privacy products or none

--- plugins/sk-privacy-products/includes/class-sk-privacy-products.php
+++ plugins/sk-privacy-products/includes/class-sk-privacy-products.php
@@ -24,10 +24,61 @@ class SK_Privacy_Products {
 	private function init_hooks() {
 		add_action( 'woocommerce_product_options_general_product_data', array( $this, 'add_custom_product_field' ) );
 		add_action( 'woocommerce_process_product_meta', array( $this, 'add_custom_product_field_save' ) );
-		add_action( 'woocommerce_order_status_changed', array( $this, 'remove_privacy_data' ), 10, 3 );
+		add_action( 'woocommerce_order_status_changed', array( $this, 'remove_privacy_data' ), 100, 3 );
 
 		add_action( 'woocommerce_single_product_summary', array( $this, 'display_privacy_info'), 15 );
 		add_action( 'woocommerce_after_shop_loop_item_title', array( $this, 'display_privacy_info'), 15 );
+
+		add_filter( 'woocommerce_add_to_cart_validation', array($this, 'add_to_cart_validation'), 10, 5 );
+	}
+
+	function add_to_cart_validation( $passed, $product_id, $quantity, $variation_id = '', $variations= '' ) {
+
+		if( !$passed ) return false;
+
+		global $woocommerce;
+
+		$items = $woocommerce->cart->get_cart();
+
+		$privacy_in_cart = false;
+
+		if ( !empty( $items ) ) {
+
+			foreach( $items as $item ) {
+				// Check if any product has been set to privacy
+				if( 'yes' == get_post_meta( $item['product_id'], '_privacy_enabled', true ) ) {
+					$privacy_in_cart = true;
+				}
+			}
+
+		}
+
+		$is_privacy = ( 'yes' == get_post_meta( $product_id, '_privacy_enabled', true ) );
+
+		if ( count($items) < 1 ) {
+			// Cart empty, product can be added
+			$passed = true;
+		}
+
+		if ( true == $is_privacy && true == $privacy_in_cart ) {
+			// Privacy product can be added to cart with privacy items.
+			$passed = true;
+		}
+
+		if ( false == $is_privacy && true == $privacy_in_cart ) {
+			// Non privacy product can not be added to cart with privacy items.
+			$passed = false;
+			wc_add_notice('Varukorgen innehåller sekretessvaror, slutför ordern med dessa innan du köper denna vara.', 'error');
+		}
+
+		if ( true == $is_privacy && (false == $privacy_in_cart && count( $items ) > 0 ) ) {
+			// Privacy product can not be added to cart with non privacy products.
+			$passed = false;
+			wc_add_notice('Varukorgen innehåller varor som inte är sekretessmarkerade, slutför ordern med dessa innan du köper denna sekretessvara.', 'error');
+		}
+
+		return $passed;
+
 	}
 
 	/**
@@ -38,12 +89,12 @@ class SK_Privacy_Products {
 
 		echo '<div class="options_group">';
 
-		woocommerce_wp_checkbox( 
-			array( 
+		woocommerce_wp_checkbox(
+			array(
 				'id'            => '_privacy_enabled',
 				'wrapper_class' => 'show_if_simple',
-				'label'         => __('Sekretess', 'skpp' ), 
-				'description'   => __( 'Efter lagt order som innehåller denna produkt rensas all produkt- och kundinformation från ordern. E-postnotiser med information går iväg som vanligt innan detta sker.', 'skpp' ) 
+				'label'         => __('Sekretess', 'skpp' ),
+				'description'   => __( 'Efter lagt order som innehåller denna produkt rensas all produkt- och kundinformation från ordern. E-postnotiser med information går iväg som vanligt innan detta sker.', 'skpp' )
 			)
 		);
 
@@ -114,10 +165,9 @@ class SK_Privacy_Products {
 
 	}
 
-	public function remove_privacy_data( $order_id, $old_status, $new_status ) {
+	private function order_contains_privacy_products( $order_id ) {
 
-		// Only check for privacy products on internal orders.
-		if ( $new_status !== 'internal-order' ) return;
+		if ( !$order_id ) return false;
 
 		$privacy = false;
 
@@ -136,11 +186,20 @@ class SK_Privacy_Products {
 
 		}
 
+		return $privacy;
+	}
+
+	public function remove_privacy_data( $order_id, $old_status, $new_status ) {
+
+		// Only check for privacy products on internal orders.
+		if ( $new_status !== 'internal-order' ) return;
+
 		// If no products has been set to privacy, do nothing.
-		if (!$privacy) {
+		if( !$this->order_contains_privacy_products($order_id) ) {
 			return;
 		}
 
+		$order = new WC_Order( $order_id );
 
 		/**
 		 * Remove address info from order.
