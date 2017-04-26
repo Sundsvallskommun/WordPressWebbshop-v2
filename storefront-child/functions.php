c52dd9d49c0f048a2e55d9cbba1450fa812a539b
Johan Linder <johan@fmca.se>
2017-04-26 16:31:48 +0200

    First version of togglable categories

--- storefront-child/functions.php
+++ storefront-child/functions.php
@@ -1,2 +1,43 @@
 <?php
 
+add_action( 'wp_footer', 'collapsable_categories' );
+
+function collapsable_categories() {
+  ?>
+
+    <style>
+    .widget_product_categories .cat-parent.closed .children {
+      display: none;
+    }
+
+    .widget_product_categories .cat-parent:before {
+      content: "- \f114"
+    }
+
+    .widget_product_categories .cat-parent:before {
+      cursor: pointer;
+    }
+
+    .widget_product_categories .cat-parent:before {
+      content: "\f115" !important;
+    }
+
+    .widget_product_categories .cat-parent.closed:before {
+      content: "\f114" !important;
+    }
+
+    </style>
+    <script>
+      jQuery( document ).ready( function($) {
+
+        $( '.widget_product_categories' ).on( 'click', '.cat-parent', function(e) {
+
+          if (e.target !== this) return;
+          
+          $(this).toggleClass('closed');
+        });
+
+      });
+    </script>
+  <?php
+}
