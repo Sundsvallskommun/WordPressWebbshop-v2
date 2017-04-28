18d8768e8d7f052e21e035ed2ad39d9dfdb28fc1
Rickard Karlsson <rickard@fmca.se>
2017-04-28 15:00:05 +0200

    made billing_company readonly in checkout

--- plugins/sk-smex/includes/class-sk-smex.php
+++ plugins/sk-smex/includes/class-sk-smex.php
@@ -162,7 +162,7 @@ class SK_SMEX {
 	public function set_readonly_checkout_fields( $args, $key, $value ) {
 		// Only modify on checkout.
 		if ( is_checkout() ) {
-			if ( $key === 'billing_first_name' || $key === 'billing_last_name' ) {
+			if ( $key === 'billing_first_name' || $key === 'billing_last_name' || $key === 'billing_company' ) {
 				$args[ 'custom_attributes' ][ 'readonly' ] = 'readonly';
 			}
 		}
