f2de6a7e0f15ebda2da4900d1bc0e45049334e1e
Johan Linder <johan@fmca.se>
2017-10-06 11:39:19 +0200

    Overwrite form submissions for privacy nistead of deleting it

--- plugins/sk-webshop/includes/class-sk-gf-privacy.php
+++ plugins/sk-webshop/includes/class-sk-gf-privacy.php
@@ -76,7 +76,42 @@ class SK_GF_Privacy {
 
 		if ( $is_privacy !== '1' ) return;
 
-		GFAPI::delete_entry( $entry['id'] );
+    // Uncomment the following two lines to delete the entry instead of modifying it.
+    // GFAPI::delete_entry( $entry['id'] );
+    // return;
+
+    $updated_entry = $entry;
+
+    $allowed_values = array(
+      'id',
+      'form_id',
+      'date_created',
+      'is_read',
+      'source_url',
+      'post_id',
+      'currency',
+      'payment_status',
+      'payment_date',
+      'transaction_id',
+      'payment_amount',
+      'payment_method',
+      'is_fulfilled',
+      'transaction_type',
+      'status'
+    );
+
+    foreach ( $updated_entry as $key => $value ) {
+      if (!in_array( $key, $allowed_values )) {
+        $updated_entry[$key] = 'Sekretess';
+      }
+
+      // Set user ID to a high integer probably not used by any user.
+      if ( $key == 'created_by') {
+        $updated_entry[$key] = '2000000000';
+      }
+    }
+
+		GFAPI::update_entry( $updated_entry, $entry['id'] );
 
 	}
 
