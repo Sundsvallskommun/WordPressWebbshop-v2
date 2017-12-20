51bdf21e6a239e74557ff6c29ecf0980997ceaa8
Rickard Karlsson <rickard@fmca.se>
2017-12-20 15:05:46 +0100

    Fix: cleaned up the code and increased compabatility with WP code standards Fix: added more optional fields

--- plugins/sk-webshop/includes/class-sk-webshop-required-fields.php
+++ plugins/sk-webshop/includes/class-sk-webshop-required-fields.php
@@ -12,21 +12,45 @@ class SK_Webshop_Required_Fields {
 	 * The post meta key name.
 	 * @var string
 	 */
-	private static $FIELDS_META_KEY = 'sk_checkout_tab_fields';
+	private static $fields_meta_key = 'sk_checkout_tab_fields';
 
 	/**
 	 * The fields that we are interested in.
 	 * @var array
 	 */
-	private $FIELDS = array(
-		'billing_department'		=> array(
-			'label' => 'Arbetsplats, avdelning, rum'
+	private $fields = array(
+		'billing_organization' => array(
+			'label' => 'Förvaltning/bolag',
 		),
-		'billing_address_1'		=> array(
-			'label' => 'Gatuadress'
+		'billing_department' => array(
+			'label' => 'Arbetsplats, avdelning, rum',
 		),
-		'billing_organization'		=> array(
-			'label' => 'Förvaltning/bolag'
+		'billing_address_1' => array(
+			'label' => 'Gatuadress',
+		),
+		'billing_postcode' => array(
+			'label' => 'Postnummer',
+		),
+		'billing_city' => array(
+			'label' => 'Ort',
+		),
+		'billing_reference_number' => array(
+			'label' => 'Referensnummer',
+		),
+		'billing_responsibility_number' => array(
+			'label' => 'Ansvarsnummer',
+		),
+		'billing_occupation_number' => array(
+			'label' => 'Verksamhetsnummer',
+		),
+		'billing_activity_number' => array(
+			'label' => 'Aktivitetsnummer',
+		),
+		'billing_project_number' => array(
+			'label' => 'Projektnummer',
+		),
+		'billing_object_number' => array(
+			'label' => 'Objektsnummer',
 		),
 	);
 
@@ -35,9 +59,9 @@ class SK_Webshop_Required_Fields {
 	 * correct array when all checkboxes are unticked.
 	 * @var array
 	 */
-	private $FIELDS_UNCHECKED = array(
-		'billing_address_1'		=> 'no',
-		'billing_department'		=> 'no'
+	private $fields_unchecked = array(
+		'billing_address_1'  => 'no',
+		'billing_department' => 'no',
 	);
 
 	/**
@@ -47,11 +71,10 @@ class SK_Webshop_Required_Fields {
 		// Change fields.
 		add_filter( 'woocommerce_checkout_fields', array( $this, 'change_required' ), 9999999, 1 );
 		add_filter( 'woocommerce_default_address_fields', array( $this, 'change_required' ), 9999999, 1 );
+
 		// Add the checkout tab to product data tabs.
 		add_filter( 'woocommerce_product_data_tabs', array( $this, 'add_checkout_tab' ), 99, 1 );
-
 		add_action( 'woocommerce_product_data_panels', array( $this, 'add_checkout_tab_fields' ) );
-
 		add_action( 'save_post', array( $this, 'save_checkout_tab_fields' ) );
 	}
 
@@ -62,10 +85,10 @@ class SK_Webshop_Required_Fields {
 	 * @return array
 	 */
 	public function add_checkout_tab( $tabs ) {
-		$tabs[ 'sk-checkout-tab' ] = array(
-			'label'		=> __( 'Kassavy', 'sk-shop' ),
-			'target'	=> 'sk_checkout_product_data_tab',
-			'class'		=> '', // index 'class' must be defined. Docs don't mention why.
+		$tabs['sk-checkout-tab'] = array(
+			'label'  => __( 'Kassavy', 'sk-shop' ),
+			'target' => 'sk_checkout_product_data_tab',
+			'class'  => '', // index 'class' must be defined. Docs don't mention why.
 		);
 
 		return $tabs;
@@ -87,23 +110,22 @@ class SK_Webshop_Required_Fields {
 
 			<?php
 			// Get the saved values.
-		$values = get_post_meta( $post->ID, self::$FIELDS_META_KEY, true );
-
+			$values = get_post_meta( $post->ID, self::$fields_meta_key, true );
 
 			// Create text inputs for each fields.
-			foreach ( $this->FIELDS as $field => $field_data ) {
+			foreach ( $this->fields as $field => $field_data ) {
 				// Check if we have a saved value.
 				$value = ( $values && ! empty( $values[ $field ] ) ) ? $values[ $field ] : 'no';
 
 				// Create field.
 				woocommerce_wp_checkbox( array(
-					'id'			=> self::$FIELDS_META_KEY . '[' . $field . ']',
-					'wrapper_class'	=> 'show_if_simple',
-					'label'			=> $field_data['label'],
-					'description'	=> 'Inaktivera tvingande för ' . $field_data['label'],
-					'default'		=> 'no',
-					'value'			=> $value,
-					'desc_tip'		=> false,
+					'id'            => self::$fields_meta_key . '[' . $field . ']',
+					'wrapper_class' => 'show_if_simple',
+					'label'         => $field_data['label'],
+					'description'   => 'Inaktivera fältet ' . $field_data['label'],
+					'default'       => 'no',
+					'value'         => $value,
+					'desc_tip'      => false,
 				) );
 			}
 			?>
@@ -124,11 +146,11 @@ class SK_Webshop_Required_Fields {
 		}
 
 		// Save values or default value
-		$fields = ( ! empty( $_REQUEST[ self::$FIELDS_META_KEY ] ) ) ? $_REQUEST[ self::$FIELDS_META_KEY ] : '';
-		if( ! empty( $fields ) ) {
-			update_post_meta( $post_id, self::$FIELDS_META_KEY, $fields );
+		$fields = ( ! empty( $_REQUEST[ self::$fields_meta_key ] ) ) ? $_REQUEST[ self::$fields_meta_key ] : '';
+		if ( ! empty( $fields ) ) {
+			update_post_meta( $post_id, self::$fields_meta_key, $fields );
 		} else {
-			update_post_meta( $post_id, self::$FIELDS_META_KEY, $this->FIELDS_UNCHECKED );
+			update_post_meta( $post_id, self::$fields_meta_key, $this->fields_unchecked );
 		}
 	}
 
@@ -138,65 +160,61 @@ class SK_Webshop_Required_Fields {
 	 * @return array
 	 */
 	public function change_required( $fields ) {
-
 		global $woocommerce;
 		$items = $woocommerce->cart->get_cart();
 
-		$_fields = $this->FIELDS;
+		if ( empty( $items ) ) {
+			return $fields;
+		}
 
-		foreach($items as $item => $values) {
+		$_fields = $this->fields;
 
+		foreach ( $items as $item => $values ) {
 			$product_id = $values['product_id'];
-			$opt_fields = (array)get_post_meta( $product_id, self::$FIELDS_META_KEY, true );
+			$opt_fields = (array) get_post_meta( $product_id, self::$fields_meta_key, true );
 
-			$opt_fields = array_filter( $opt_fields, function($v) {
-				return $v == 'yes';
+			$opt_fields = array_filter( $opt_fields, function( $v ) {
+				return 'yes' === $v;
 			});
 
-			foreach ( $_fields as $field => $field_data) {
+			foreach ( $_fields as $field => $field_data ) {
+				$shouldbe_required = array_key_exists( $field, $opt_fields ) ? false : true;
 
-				$shouldbe_required = array_key_exists($field, $opt_fields) ? false : true;
-				$is_required = $field_data['required'] || false;
-
-				if ( $shouldbe_required && !$is_required ) {
-					$_fields[$field]['required'] = true;
-				}
-
+				$_fields[ $field ]['required'] = $shouldbe_required;
 			}
-
 		}
 
-		if (isset($fields['billing'])) {
-
+		if ( isset( $fields['billing'] ) ) {
 			foreach ( $fields['billing'] as $field => $value ) {
-				if(!is_null($_fields[$field])) {
-					$required = $_fields[$field]['required'];
-						$fields['billing'][ $field ][ 'required' ]	= $required;
-						if($required == false) {
-							unset($fields['billing'][ $field ]);
-						}
+				if ( isset( $_fields[ $field ] ) ) {
+					$required = $_fields[ $field ]['required'];
+					$fields['billing'][ $field ]['required'] = $required;
+
+					if ( ! $required ) {
+						unset( $fields['billing'][ $field ] );
 					}
+				}
 			}
-
 		} else {
-
 			foreach ( $fields as $field => $value ) {
 				$_field = substr( $field, 0, 8 ) === 'billing_' ? $field : 'billing_' . $field;
-				if(!is_null($_fields[$_field])) {
-					$required = $_fields[$_field]['required'];
-						if ($required == false) {
-							$fields[ $field ][ 'required' ]	= $required;
-							if($required == false) {
-								unset($fields[ $field ]);
-							}
+
+				if ( isset( $_fields[ $_field ] ) ) {
+					$required = $_fields[ $_field ]['required'];
+
+					if ( ! $required ) {
+						$fields[ $field ]['required'] = $required;
+
+						if ( ! $required ) {
+							unset( $fields[ $field ] );
 						}
 					}
+				}
 			}
-
 		}
 
-			// Return new fields.
-			return $fields;
+		// Return new fields.
+		return $fields;
 	}
 
 }
