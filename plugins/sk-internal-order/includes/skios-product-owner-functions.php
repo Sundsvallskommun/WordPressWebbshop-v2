8635d0f012a42d72fb9bb4aca0dfc251193d2b3a
Johan Linder <johan@fmca.se>
2020-10-15 09:08:03 +0200

    Add info to SKIOS order email:
    - Add comma separated string for tele2.
    - Add comma separated "verksamhetsbeskrivning".

--- plugins/sk-internal-order/includes/skios-product-owner-functions.php
+++ plugins/sk-internal-order/includes/skios-product-owner-functions.php
@@ -372,12 +372,62 @@ function skios_email_items( $order, $items ) {
 								$string .= sprintf( '<br><strong>%s</strong>: %s <br>', $meta->key, $meta->value );
 							}
 							$string .= '<br>';
+							$string .= '<strong>Tele2 kostnadsställe: </strong>';
+							$string .= get_tele2_string( $order, $item );
+							$string .= '<br>';
+							$string .= '<strong>Servicecenter IT kostnadsställe: </strong>';
 							$string .= get_pob_string( $order, $item );
+							$string .= '<br>';
+							$string .= '<strong>Verksamhetsbeskrivning: </strong>';
+							$string .= get_occupation_string( $order, $item );
 					$string .= '</td>';
 				$string .= '</tr>';
 			}
 		$string .= '</tbody>';
 	$string .= '</table>';
+
+	return $string;
+}
+
+function get_occupation_string( $order, $item ) {
+	$string  = "<span id='occupationString'>";
+	$values = [];
+
+	$values[] = $order->get_meta( '_billing_organization', true );
+	$values[] = $order->get_meta( '_billing_department', true );
+	$values[] = $order->get_billing_first_name() . ' ' .  $order->get_billing_last_name();
+
+	$string .= implode( ', ', $values );
+	$string .= '</span>';
+
+	return $string;
+}
+
+function get_tele2_string( $order, $item ) {
+
+	$pob_string_data = get_pob_string_data( $order, $item );
+
+	error_log( $pob_string_data );
+
+	$string  = "<span id='tele2String'>";
+
+	$fields = array(
+		'billing_responsibility_number',
+		'billing_occupation_number',
+		'billing_activity_number',
+		'billing_project_number',
+		'billing_object_number',
+	);
+
+	$values = [];
+
+	foreach ( $fields as $key ) {
+		$values[] = $pob_string_data[ $key ][ 'meta_val' ];
+	}
+
+	// Add a space between every data field.
+	$string .= implode( ',', $values );
+	$string .= '</span>';
 
 	return $string;
 }
