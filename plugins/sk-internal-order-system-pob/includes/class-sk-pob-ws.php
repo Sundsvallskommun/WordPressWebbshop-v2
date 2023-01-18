92b20213451072f42aceae0a89a849390ad15c16
Sebastian Åhman <99800710+sebahm@users.noreply.github.com>
2023-01-18 14:02:09 +0100

    Revert "add sections from pob order to memo"

--- plugins/sk-internal-order-system-pob/includes/class-sk-pob-ws.php
+++ plugins/sk-internal-order-system-pob/includes/class-sk-pob-ws.php
@@ -131,20 +131,7 @@ class Sk_POB_WS {
 
 				$meta = $item->get_meta_data();
 
-				if ( isset($form_section) ){
-					foreach ($form_section as $field){
-						$memo .= "<strong>$field->label</strong><br/><br/>";
-					}
-				}
-				foreach ($form['fields'] as $field) {
-					if ($field->type == 'section'){
-						$memo .= "<strong>$field->label</strong><br/><br/>";
-					}
-					$m = $this->get_meta_by_key($field->label, $meta);
-					if (!$m){
-						continue;
-					}
-					
+				foreach ($meta as $m) {
 					$meta_label = $m->get_data()['key'];
 					$pob_id = $this->get_pob_id($item, $meta_label);
 
@@ -165,15 +152,6 @@ class Sk_POB_WS {
 				$count++;
 			}
 		}
-	}
-
-	private function get_meta_by_key($key, $meta){
-		foreach($meta as $m){
-			if($key == $m->get_data()['key']){
-				return $m;
-			}
-		}
-		return false;
 	}
 
 	private function get_pob_id($item, $field_label)
